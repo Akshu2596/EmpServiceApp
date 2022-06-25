c6554e52ed64db1bdc99d06d74fbec81e0c7eef2
“Moriarty” <Akankshaverma005@gmail.com>
2022-06-25 11:36:40 +0530

    added services and models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from './services/employee.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'EmpServiceApp';
+
+constructor(private employeeService: EmployeeService){
+
+
+}
+//how the app loads the very first time
+  ngOnInit(): void {
+    this.getAllEmployees();
+  }
+  getAllEmployees(){
+    this.employeeService.getAllEmployees()
+    .subscribe(
+      response => {
+        console.log(response);
+      }
+    );
+  }
 }
--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../models/employee.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
-  constructor() { }
+  serviceUrl ='https://localhost:5001/api/employee'
+  constructor(private http: HttpClient) { }
+
+  //Get all employees
+
+  getAllEmployees(): Observable<Employee[]> {
+    
+    return this.http.get<Employee []>(this.serviceUrl);
+  }
 }
